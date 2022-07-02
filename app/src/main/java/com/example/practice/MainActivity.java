f6bc7c10515a14af4638b6c5e6147b4c5a023a7e
kolyabik <nikolai.yalok@gmail.com>
2022-07-02 19:14:16 +0300

    Эстетичесие правки

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -5,27 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button toSchedule;
+    Button toInformation;
+    Button toContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toSchedule = (Button) findViewById(R.id.toShedule);
+        toContacts = (Button) findViewById(R.id.toContacts);
+        toInformation = (Button) findViewById(R.id.toInformation);
+
+        toContacts.setOnClickListener(this);
+        toInformation.setOnClickListener(this);
+        toSchedule.setOnClickListener(this);
     }
 
-    public void toSchedule(View view) {
-        Intent intent = new Intent(this,Schedule.class);
-        startActivity(intent);
-    }
 
-    public void toContact(View view) {
-        Intent intent = new Intent(this,Contacts.class);
-        startActivity(intent);
-    }
 
-    public void toInformation(View view) {
-        Intent intent = new Intent(this,Information.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()){
+            case R.id.toContacts:
+                intent = new Intent(this, Contacts.class);
+                break;
+            case R.id.toInformation:
+                intent = new Intent(this,Information.class);
+                break;
+            case R.id.toShedule:
+                intent = new Intent(this,Schedule.class);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/practice/Schedule.java
+++ app/src/main/java/com/example/practice/Schedule.java
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Schedule extends AppCompatActivity {
+public class Schedule extends AppCompatActivity implements View.OnClickListener {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        start = (Button) findViewById(R.id.toStart);
+
+        start.setOnClickListener(this);
     }
 
-    public void toStart(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()){
+            case R.id.toStart:
+                intent = new Intent(this, MainActivity.class);
+                break;
+        }
+
     }
 }
